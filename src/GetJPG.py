074b3324347c85e9031fa84ca777ba677c26803d
puccapy <43285319@qq.com>
2017-09-25 15:14:07 +0800

    0925

--- src/GetJPG.py
+++ src/GetJPG.py
@@ -8,21 +8,29 @@ import urllib
 import re
 class GetIMG:
     def __init__(self):
-        self.url="https://www.yyfax.com/"
+        self.url="https://www.ricequant.com/community/topic/2855/"
     def getHtml(self):
         page = urllib.urlopen(self.url)
         html = page.read()
         return html
 
     def getImg(self,html):
-        reg = r'src="(.+?\.jpg)"'
+        reg = r'src="(.+?\.jpg|.+?\.gif|.+?\.png)"'
         imgre = re.compile(reg)
         imglist = re.findall(imgre,html)
         x = 0
         for imgurl in imglist:
-            newimgurl=self.url+imgurl
+            if imgurl.find('http')<0:
+                newimgurl=self.url+imgurl
+            else:              
+                newimgurl=imgurl
             print newimgurl
-            urllib.urlretrieve(newimgurl,'%s.jpg' % x)
+            if newimgurl.find('jpg')>0:
+                urllib.urlretrieve(newimgurl,'%s.jpg' % x)
+            elif newimgurl.find('gif')>0:
+                urllib.urlretrieve(newimgurl,'%s.gif' % x)
+            else:
+                urllib.urlretrieve(newimgurl,'%s.png' % x)
             x+=1
 
 if __name__=='__main__':
